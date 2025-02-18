5abc2046cdce088eb4ef7ba691ebcdb0adcae082
Matheus-Ernesto <matheusernestodossantos231@gmail.com>
2025-02-18 08:40:18 -0300

    updated and finished - TwoSum

--- scripts/src/leetcodeJava/TwoSum.java
+++ scripts/src/leetcodeJava/TwoSum.java
@@ -6,24 +6,13 @@ import java.util.List;
 public class TwoSum {
 	public List<Integer> twoSum(List<Integer> input, int target) {
 		List<Integer> result = new ArrayList<>();
-		Integer indexOne = 0;
-		Integer indexTwo = 0;
-		for(int inputOne : input) {
-			for(int inputTwo : input) {
-				if((inputOne + inputTwo) == target) {
-					result.add(indexOne);
-					result.add(indexTwo);
+		for(Integer index1=0; index1 < input.size(); index1++) {
+			for(Integer index2=index1; index2 < input.size(); index2++) {
+				Integer value = input.get(index1) + input.get(index2);
+				if(value == target) {
+					result.add(index1);
+					result.add(index2);
 				}
-				indexTwo ++;
-			}
-			indexOne ++;
-			indexTwo = 0;
-		}
-		int size = (result.size() > 0) ? result.size()-2 : 0;
-		for(int cont = 0; cont < size;cont+=2) {
-			if(result.get(cont) == result.get(cont+3)) {
-				result.remove(cont+3);
-				result.remove(cont+2);
 			}
 		}
 		return result;
